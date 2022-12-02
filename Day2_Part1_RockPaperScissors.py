b8cd5ba0b2d135f0b8ac85f2f7da3b52f1098721
jessie-black <jess.blk@gmail.com>
2022-12-01 21:07:47 -0800

    Parameters & input for day 2 part 1

---
+++ Day2_Part1_RockPaperScissors.py
@@ -0,0 +1,6 @@
+# Given a strategy guide (input file) with the following info:
+# COLUMN 1 = what opponent will play (A = Rock, B = Paper, C = Scissors)
+# COLUMN 2 = what you should play in response (X = Rock, Y = Paper, Z = Scissors)
+# Total score = sum of scores for each round
+# Score for round = shape you selected (1=Rock, 2=Paper, 3=Scissors) + score for outcome (0 = lost, 3 = draw, 6 = won)
+# What score would you get IF you followed strategy guide?
