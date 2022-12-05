2a47657055b6d33b0f5d07c1bb962d50d37f190e
jessie-black <jess.blk@gmail.com>
2022-12-04 21:48:56 -0800

    Cleaned up Day 5 code for readability

--- Day5_Part1.py
+++ Day5_Part1.py
@@ -1,31 +1,24 @@
-#
-#
-#
-#
-#
-# Create a function that moves top of one stack to top of another
-# I only need to pop final items off of stacks in order.
+# Read text in put as visual representation of 9 stacks (string format)
+# move top N values of one stack onto the top of another - one item is moved at a time (order reversed)
+# Final result = top value from each stack at end of transformations
 stacks={1:[],2:[],3:[],4:[],5:[],6:[],7:[],8:[],9:[]}
 file_path = 'day5_input.txt' # initial stack setup and instructions
 with open(file_path) as input_file:
     original_stacks = input_file.readlines()
-for i in range(8): # traverse stacks from bottom to top line by line
-    # stacks[1].append(original_stacks[7-i][1])
+for i in range(8): # traverse stacks from bottom to top line by line to populate stacks
     for j in range(9):
         if original_stacks[7-i][(j*4)+1]!=" ":
             stacks[j+1].append(original_stacks[7-i][(j*4)+1])
-# at this point i have all the stacks
+# All stacks now represented in list form
 
-
-#START AT LINE 10
-# Read instructions
+# Starting at line 10, read all instructions for movement between stacks
 for i in range(len(original_stacks)-10):
     current_instruction = (original_stacks[i+10])
     current_instruction = current_instruction.strip().split(" ")
-    number=int(current_instruction[1])
-    origin=int(current_instruction[3])
-    destination=int(current_instruction[5])
+    number=int(current_instruction[1]) # how many items to move
+    origin=int(current_instruction[3]) # from origin stack
+    destination=int(current_instruction[5]) #to destination stack
     for i in range(number):
         stacks[destination].append(stacks[origin].pop())
-for i in range(9):
-    print(stacks[i+1])
+for i in range(9): #get top item from each stack
+    print(stacks[i+1].pop(),end="")
--- Day5_Part2.py
+++ Day5_Part2.py
@@ -1,34 +1,27 @@
-#
-#
-#
-#
-#
-# Create a function that moves top of one stack to top of another
-# I only need to pop final items off of stacks in order.
+# Read text in put as visual representation of 9 stacks (string format)
+# move top N values of one stack onto the top of another - all items moved at once (order conserved)
+# Final result = top value from each stack at end of transformations
 stacks={1:[],2:[],3:[],4:[],5:[],6:[],7:[],8:[],9:[]}
 file_path = 'day5_input.txt' # initial stack setup and instructions
 with open(file_path) as input_file:
     original_stacks = input_file.readlines()
-for i in range(8): # traverse stacks from bottom to top line by line
-    # stacks[1].append(original_stacks[7-i][1])
+for i in range(8): # traverse stacks from bottom to top line by line to populate stacks
     for j in range(9):
         if original_stacks[7-i][(j*4)+1]!=" ":
             stacks[j+1].append(original_stacks[7-i][(j*4)+1])
-# at this point i have all the stacks
+# All stacks now represented in list form
 
-
-#START AT LINE 10
-# Read instructions
+# Starting at line 10, read all instructions for movement between stacks
+temp_stack=[] # temp stack to preserve order in final move
 for i in range(len(original_stacks)-10):
     current_instruction = (original_stacks[i+10])
     current_instruction = current_instruction.strip().split(" ")
     number=int(current_instruction[1])
     origin=int(current_instruction[3])
     destination=int(current_instruction[5])
-    temp_stack=[]
-    for i in range(number):
+    for i in range(number): #move items to temporary stack (reversed order)
         temp_stack.append(stacks[origin].pop())
-    for i in range(number):
+    for i in range(number): #move items to final stack (original order preserved)
         stacks[destination].append(temp_stack.pop())
-for i in range(9):
-    print("test",stacks[i+1])
+for i in range(9): #get top item from each stack
+    print(stacks[i+1].pop(),end="")
