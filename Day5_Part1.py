42dc0461499956bbf5cd7edc9f3029880de8a252
jessie-black <jess.blk@gmail.com>
2022-12-04 21:36:29 -0800

    Day 5 part 1 works

--- Day5_Part1.py
+++ Day5_Part1.py
@@ -3,12 +3,9 @@
 #
 #
 #
-
 # Create a function that moves top of one stack to top of another
-
 # I only need to pop final items off of stacks in order.
 stacks={1:[],2:[],3:[],4:[],5:[],6:[],7:[],8:[],9:[]}
-stack1 = stack2 = stack3 = stack4 = stack5 = stack6 = stack7 = stack8 = stack9 = []
 file_path = 'day5_input.txt' # initial stack setup and instructions
 with open(file_path) as input_file:
     original_stacks = input_file.readlines()
@@ -18,14 +15,17 @@ for i in range(8): # traverse stacks from bottom to top line by line
         if original_stacks[7-i][(j*4)+1]!=" ":
             stacks[j+1].append(original_stacks[7-i][(j*4)+1])
 # at this point i have all the stacks
+
+
+#START AT LINE 10
+# Read instructions
+for i in range(len(original_stacks)-10):
+    current_instruction = (original_stacks[i+10])
+    current_instruction = current_instruction.strip().split(" ")
+    number=int(current_instruction[1])
+    origin=int(current_instruction[3])
+    destination=int(current_instruction[5])
+    for i in range(number):
+        stacks[destination].append(stacks[origin].pop())
 for i in range(9):
     print(stacks[i+1])
-# 1 = [1]
-# 2 = [5]
-# 3 = [9]
-# 4 = [13]
-# 5 = [17]
-# 6 = [21]
-# 7 = [25]
-# 8 = [29]
-# 9 = [33]
